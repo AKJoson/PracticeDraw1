c347ef3cd169de6aa62475010424e8ede96cda94
yinpeng <yinpeng6868@gmail.com>
2019-02-25 22:27:43 +0800

    in you drawArc,the swepAngle is range of angle not the end angle.

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,28 +2,44 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
+    private Paint mTopArcPaint, mLinePaint;
 
     public Practice8DrawArcView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Practice8DrawArcView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Practice8DrawArcView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mTopArcPaint = new Paint();
+        mLinePaint = new Paint();
+        mTopArcPaint.setColor(Color.BLACK);
+        mLinePaint.setColor(Color.BLACK);
+        mLinePaint.setStyle(Paint.Style.STROKE);
+        mLinePaint.setStrokeWidth(2);
+        mTopArcPaint.setAntiAlias(true);
+        mLinePaint.setAntiAlias(true);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        RectF rectF = new RectF(300, 200, 600, 500);
+        RectF rectF1 = new RectF(300, 300, 600, 500);
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        canvas.drawArc(rectF, 190, 50, false, mLinePaint);
+        canvas.drawArc(rectF, 260, 90, true, mTopArcPaint);
+       canvas.drawArc(rectF1, 0, 180, true, mTopArcPaint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -1,23 +1,39 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+    private Paint mPaint;
+    private Path path;
 
     public Practice9DrawPathView(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,0);
     }
 
+
+    @SuppressLint("NewApi")
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        path = new Path();
+        mPaint.setColor(Color.BLACK);
+        path.addArc(200, 200, 400, 400, -225, 225);
+        path.arcTo(400, 200, 600, 400, -180, 225, false);
+        path.lineTo(400, 542);
     }
 
     @Override
@@ -25,5 +41,6 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        canvas.drawPath(path,mPaint);
     }
 }
